a3fa37862e2fb42aa79a702826acd2c00348e5b0
MFathurrohmanHaris <harisfathurrohman142@gmail.com>
2022-12-07 13:30:43 +0700

    Menyelesaikan pembuatan menu/home

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg" style="background: #F4DCD6">
+  <div class="container">
+    <a class="navbar-brand" href="#">Laraboo Team/Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Category</a>
+        </li>
+        <li class="nav-item dropdown">
+        </li>
+      </ul>
+      <form class="d-flex me-auto" role="search">
+        <input class="form-control me-2 col-lg-8" type="search" placeholder="Search" aria-label="Search">
+        <button class="buttonSendiri" type="submit">Search</button>
+      </form>
+      <ul class="navbar-nav mb-2 me-5 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
