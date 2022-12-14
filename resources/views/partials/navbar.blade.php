d73393f6027a6755cdcfb74c08230359e5df9b93
MFathurrohmanHaris <harisfathurrohman142@gmail.com>
2022-12-14 12:33:34 +0700

    Edit Tampilan Menu

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg" style="background: #F4DCD6">
     <div class="container">
-        <a class="navbar-brand" href="#">Laraboo Team/Logo</a>
+        <a class="navbar-brand" href="#"><img src="img/logo2.png" width="56" height="45" alt=""></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
