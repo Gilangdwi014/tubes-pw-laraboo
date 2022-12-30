0ae71a23a49464d7dc52710639acac44e5594dda
MFathurrohmanHaris <harisfathurrohman142@gmail.com>
2022-12-30 10:43:44 +0700

    mengubah halaman About

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,15 +1,58 @@
 @extends('layouts.main')
 @section('container')
-    <center><h1><img src="img/logo2.png" width="100" alt="">Cooking Master</h1></center>
-    <center><p>Website Cooking Master merupakan website yang menyediakan berbagai macam resep masakan, 
-        mulai dari menu utama, minuman sampai ke hidangan penutup/dessert, 
-        Pengguna bisa melihat berbagai macam resep, cara pembuatan, hingga feedback pengguna lain. 
-        Fitur bookmark akan memudahkan anda dalam menyimpan resep masakan, sehingga anda akan lebih mudah dalam memasak.</p></center>
-        <br>
+<center><h2>About Us</h2></center>
+<div class="row">
+    <div class="col-lg-6">
         <center><img src="img/logoLaraboo.png" alt=""></center>
-        <br>
-        <center><p> Gilang Dwi Adira Karsoma 203040014
-            - Muhammad fathurrohman Haris 203040015
-            - Sulis Rosliani 203040011
-            - Dean Tirta Santika 203040009</p></center>
+    </div>
+    <div class="col-lg-6">
+        <center><img src="img/logo2.png" width="300" alt=""></center>
+    </div>
+
+    <div class="col-lg-6">
+        <p><b>Laraboo Team</b> merupakan website yang menyediakan berbagai macam resep masakan, 
+    mulai dari menu utama, minuman sampai ke hidangan penutup/dessert, 
+    Pengguna bisa melihat berbagai macam resep, cara pembuatan, hingga feedback pengguna lain. 
+    Fitur bookmark akan memudahkan anda dalam menyimpan resep masakan, sehingga anda akan lebih mudah dalam memasak.</p>
+    </div>
+    <div class="col-lg-6">
+        <p><b>Website Cooking Master</b> merupakan website yang menyediakan berbagai macam resep masakan, 
+            mulai dari menu utama, minuman sampai ke hidangan penutup/dessert, 
+            Pengguna bisa melihat berbagai macam resep, cara pembuatan, hingga feedback pengguna lain. 
+            Fitur bookmark akan memudahkan anda dalam menyimpan resep masakan, sehingga anda akan lebih mudah dalam memasak.</p>
+    </div>
+</div>
+<br><br><br>
+<center><h2>The Team</h2></center>
+<br><br><br>
+<div class="row">
+    <div class="card me-5 ms-2" style="width: 15rem;">
+        <img src="img/nophoto.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h6 class="card-title text-center">Gilang Dwi Adira Karsoma</h6>
+            <p class="card-text text-center">Project Manager</p>
+        </div>
+    </div>
+    <div class="card me-5" style="width: 15rem;">
+        <img src="img/nophoto.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h6 class="card-title text-center">Dean Tirta Santika</h6>
+            <p class="card-text text-center">Back End Developer</p>
+        </div>
+    </div>
+    <div class="card me-5" style="width: 15rem;">
+        <img src="img/nophoto.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h6 class="card-title text-center">Sulis Rosliani</h6>
+            <p class="card-text text-center">Front End Developer</p>
+        </div>
+    </div>
+    <div class="card" style="width: 15rem;">
+        <img src="img/nophoto.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h6 class="card-title text-center">M Fathurrohman Haris</h6>
+            <p class="card-text text-center">Front End Developer</p>
+        </div>
+    </div>
+</div>
 @endsection
