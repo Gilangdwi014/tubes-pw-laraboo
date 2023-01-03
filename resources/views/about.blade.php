99a42e058833793d018c4b2ab76b7e3d7568de54
Gilangdwi014 <203040014@mail.unpas.ac.id>
2023-01-03 16:04:49 +0700

    Memperbaiki footer & halaman about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,45 +10,39 @@
     </div>
 
     <div class="col-lg-6">
-        <p><b>Laraboo Team</b> merupakan website yang menyediakan berbagai macam resep masakan, 
-    mulai dari menu utama, minuman sampai ke hidangan penutup/dessert, 
-    Pengguna bisa melihat berbagai macam resep, cara pembuatan, hingga feedback pengguna lain. 
-    Fitur bookmark akan memudahkan anda dalam menyimpan resep masakan, sehingga anda akan lebih mudah dalam memasak.</p>
+        <p style="text-align: justify"><b>Laraboo Team</b> Sebuah Perusahaan developer bisnis yang ingin fokus mengembangkan produk atau layanan yang diyakini para pendirinya dibuat untuk memenuhi permintaan pasar, sehingga berusaha untuk membuat bisnis mereka menjadi yang tak tergantikan untuk pelanggan..</p>
     </div>
     <div class="col-lg-6">
-        <p><b>Website Cooking Master</b> merupakan website yang menyediakan berbagai macam resep masakan, 
-            mulai dari menu utama, minuman sampai ke hidangan penutup/dessert, 
-            Pengguna bisa melihat berbagai macam resep, cara pembuatan, hingga feedback pengguna lain. 
-            Fitur bookmark akan memudahkan anda dalam menyimpan resep masakan, sehingga anda akan lebih mudah dalam memasak.</p>
+        <p style="text-align: justify"><b>Cooking Master</b> Merupakan website yang menyediakan berbagai macam resep masakan,mulai dari menu utama, minuman sampai ke hidangan penutup/dessert, Pengguna bisa melihat Bahan baku resep,cara pembuatan resep,hingga feedback pengguna lain. Fitur bookmark akan memudahkan pengguna dalam menyimpan resep masakan,sehingga anda akan lebih mudah dalam memasak.</p>
     </div>
 </div>
 <br><br><br>
-<center><h2>The Team</h2></center>
+<center><h2>Laraboo Team</h2></center>
 <br><br><br>
 <div class="row">
     <div class="card me-5 ms-2" style="width: 15rem;">
-        <img src="img/nophoto.jpg" class="card-img-top" alt="...">
+        <img src="img/gilang.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h6 class="card-title text-center">Gilang Dwi Adira Karsoma</h6>
             <p class="card-text text-center">Project Manager</p>
         </div>
     </div>
     <div class="card me-5" style="width: 15rem;">
-        <img src="img/nophoto.jpg" class="card-img-top" alt="...">
+        <img src="img/dean.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h6 class="card-title text-center">Dean Tirta Santika</h6>
             <p class="card-text text-center">Back End Developer</p>
         </div>
     </div>
     <div class="card me-5" style="width: 15rem;">
-        <img src="img/nophoto.jpg" class="card-img-top" alt="...">
+        <img src="img/sulis.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h6 class="card-title text-center">Sulis Rosliani</h6>
             <p class="card-text text-center">Front End Developer</p>
         </div>
     </div>
     <div class="card" style="width: 15rem;">
-        <img src="img/nophoto.jpg" class="card-img-top" alt="...">
+        <img src="img/haris.jpg" class="card-img-top" alt="...">
         <div class="card-body">
             <h6 class="card-title text-center">M Fathurrohman Haris</h6>
             <p class="card-text text-center">Front End Developer</p>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,7 +8,7 @@
   </footer>
 </div> --}}
 
-<div class="container">
+{{-- <div class="container">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div class="col-md-5 d-flex align-items-center">
       <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
@@ -21,4 +21,47 @@
       <li class="ms-3"><a class="text-muted" href="#"><img src="../../../img/logo2.png" width="56" height="45" alt=""></a></li>
     </ul>
   </footer>
-</div>
+</div> --}}
+
+<footer>
+  <section id="footer" style="background-color: #D0F0C0">
+    <div class="container">
+      <div class="row" style="text-align:center;">
+        <div class="col">
+        </div>
+        <div class="col">
+
+      </div>
+      <br>
+      <p class="text-start" style="color: rgb(0, 0, 0)">Kontak & ikuti kami:</p>
+      <div class="container">
+        <p  class="text-end" style="color: rgb(0, 0, 0); float:right; padding-top:10px;">
+        <div class="col text-start">
+            <a href="https://github.com/Gilangdwi014/tubes-pw-laraboo" style="text-decoration: none;">
+              <img src="img/discord.png" alt="Github" style="height:50px; padding-left:10px; padding-right:10px;">
+            </a>
+            <a href="https://www.instagram.com/larabooteam/" style="text-decoration: none;">
+              <img src="img/instagram.png" alt="Instagram" style="height:50px; padding-left:10px; padding-right:10px;">
+            </a>
+            <a href="mailto:larabooteam@gmail.com" style="text-decoration: none;">
+              <img src="img/gmail.png" alt="Instagram" style="height:50px; padding-left:10px; padding-right:10px;">
+            </a>
+        </div>
+      </div>
+
+      <hr style="color: rgb(0, 0, 0); margin-top:15px;">
+
+      <p style="font-size: 15px; color: rgb(0, 0, 0); text-align:center; margin-top: 20px;">
+          Copyright &copy;
+          <script>
+              document.write(new Date().getFullYear());
+          </script> Cooking Master Developed By Laraboo Team Inc
+      </p>
+    </div>
+  </section>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<script src="https://apis.google.com/js/platform.js"></script>
